24ce3f791f2866c61468c922ebad9b437d5ef031
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-04 14:44:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(linenum)
   if linenum.length == 0
     puts "The line is currently empty."
   else
-    line_method_array.each.with_index(0) do |name, index|
+    line_method_array.each.with_index(1) do |name, index|
       line_method_array.push("#{index}. #{name}")
       # push method will push the name into the index
     end
