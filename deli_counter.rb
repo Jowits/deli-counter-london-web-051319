a4ab67902f60f9c68950b090e8df54575beddac0
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-04 14:42:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,5 @@
 # Write your code here.
+katz_deli = []
 def line_method(line)
   line_method_array = []
   if line.length == 0
@@ -6,8 +7,24 @@ def line_method(line)
   else
     line_method_array.each.with_index(0) do |name, index|
       line_method_array.push("#{index}. #{name}")
-      # push method will push the name into the index 
+      # push method will push the name into the index
     end
     puts "The line is: #{line_method_array.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+
+end
+
+def now_serving(nextinline)
+  if nextinline.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{nextinline[0]}."
+    nextinline.shift
   end
 end
